17f168761490eb9ff26a88290c8a31937c9a0c7c
g-akca <gok.akca66@gmail.com>
2026-04-05 15:09:29 +0300

    Add desktop responsive App and Header styles

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="text-base leading-base text-purple-900 min-h-screen flex flex-col overflow-hidden">
+    <div className="text-base desktop:text-[18px] leading-base text-purple-900 min-h-screen flex flex-col overflow-hidden">
       <Header />
 
       <main className="grow">
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,14 @@ import lightLogo from "/images/logo-light.svg";
 
 function Header() {
   return (
-    <header className="bg-purple-900 pt-8 px-5 tablet:px-11 flex justify-between">
-      <img src={lightLogo} alt="Workit logo" />
+    <header className="bg-purple-900 pt-8 desktop:pt-14 px-5 tablet:px-11 desktop:px-20 flex justify-center">
+      <nav className="w-full max-w-280 flex justify-between items-center">
+        <img src={lightLogo} alt="Workit logo" className="h-6" />
 
-      <button type="button" className="border-b-3 border-b-green text-white text-[18px] leading-base font-bold">
-        Apply for access
-      </button>
+        <button type="button" className="border-b-3 border-b-green text-white text-[18px] leading-base font-bold">
+          Apply for access
+        </button>
+      </nav>
     </header>
   )
 }
