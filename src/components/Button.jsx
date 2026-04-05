62b8e8f8a37ed9bce85447bf905d3100840bfc2a
g-akca <gok.akca66@gmail.com>
2026-04-05 13:02:47 +0300

    Add CTASection structure and styles

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 function Button({ children }) {
   return (
-    <button type="button" className="bg-green px-8 py-3 font-bold">
+    <button type="button" className="bg-green text-purple-900 px-8 py-3 font-bold">
       {children}
     </button>
   )
--- src/components/CTASection.jsx
+++ src/components/CTASection.jsx
@@ -1,7 +1,23 @@
+import Button from "./Button";
+import founderImg from "/images/image-founder.webp";
+
 function CTASection() {
   return (
-    <>
-    </>
+    <section className="pt-16 px-5 flex flex-col">
+      <img src={founderImg} alt="Founder image" className="w-70 self-center translate-y-12" />
+
+      <div className="bg-purple-900 text-white p-8 z-10 flex flex-col gap-8 text-center">
+        <h2 className="font-fraunces text-[32px] leading-[120%]">Be the first to test</h2>
+
+        <p>
+          Hi, I'm Louis Graham, the founder of the company. Book a demo call with me to become a beta tester for our app and kickstart your company. Apply for access below and I’ll be in touch to schedule a call.
+        </p>
+
+        <Button>
+          Apply for access
+        </Button>
+      </div>
+    </section>
   )
 }
 
