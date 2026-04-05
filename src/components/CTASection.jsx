dbb2c3348f30fb720df636880f4ef300f3968a10
g-akca <gok.akca66@gmail.com>
2026-04-05 14:42:37 +0300

    Add tablet responsive Button and CTASection styles

--- src/components/CTASection.jsx
+++ src/components/CTASection.jsx
@@ -3,11 +3,11 @@ import founderImg from "/images/image-founder.webp";
 
 function CTASection() {
   return (
-    <section className="pt-16 px-5 flex flex-col">
-      <img src={founderImg} alt="Founder image" className="w-70 self-center translate-y-12" />
+    <section className="pt-16 tablet:pt-0 px-5 tablet:px-11 flex flex-col">
+      <img src={founderImg} alt="Founder image" className="w-70 self-center tablet:self-start translate-y-12 tablet:translate-y-28" />
 
-      <div className="bg-purple-900 text-white p-8 z-10 flex flex-col gap-8 text-center">
-        <h2 className="font-fraunces text-[32px] leading-[120%]">Be the first to test</h2>
+      <div className="bg-purple-900 text-white p-8 tablet:p-12 z-10 flex flex-col gap-8 items-center tablet:items-start text-center tablet:text-start tablet:ml-44">
+        <h2 className="font-fraunces text-[32px] tablet:text-[48px] leading-[120%]">Be the first to test</h2>
 
         <p>
           Hi, I'm Louis Graham, the founder of the company. Book a demo call with me to become a beta tester for our app and kickstart your company. Apply for access below and I’ll be in touch to schedule a call.
