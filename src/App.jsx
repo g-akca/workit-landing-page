b4dde282ba4b4777fe6b8b6e1df995b8a06a3a14
g-akca <gok.akca66@gmail.com>
2026-04-05 12:09:19 +0300

    Add HeroSection structure and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Header from "./components/Header";
 import HeroSection from "./components/HeroSection";
-import FeaturesSection from "./components/HeroSection";
+import FeaturesSection from "./components/FeaturesSection";
 import CTASection from "./components/CTASection";
 import Footer from "./components/Footer";
 
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,7 +1,18 @@
+import heroImg from "/images/image-hero.webp";
+
 function HeroSection() {
   return (
-    <>
-    </>
+    <section className="relative bg-purple-900 rounded-b-[100%_50px] pt-14 px-5 pb-42 mb-20 text-center flex flex-col gap-10 items-center">
+      <h1 className="text-white font-fraunces font-semibold text-[48px] leading-[100%]">
+        Data <span className="underline decoration-green underline-offset-6">tailored</span> to<br/>your needs.
+      </h1>
+
+      <button type="button" className="bg-green px-8 py-3 font-bold">
+        Learn more
+      </button>
+
+      <img src={heroImg} alt="Hero image" className="w-80 h-46 absolute -bottom-20" />
+    </section>
   )
 }
 
