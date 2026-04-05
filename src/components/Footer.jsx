9e0f35255fc931fb5d78d533427792cb709ad494
g-akca <gok.akca66@gmail.com>
2026-04-05 19:11:36 +0300

    Create icon components

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,18 @@
 import darkLogo from "/images/logo-dark.svg";
-import facebookIcon from "/images/icon-facebook.svg";
-import instagramIcon from "/images/icon-instagram.svg";
-import twitterIcon from "/images/icon-twitter.svg";
+
+import FacebookIcon from "./icons/FacebookIcon";
+import TwitterIcon from "./icons/TwitterIcon";
+import InstagramIcon from "./icons/InstagramIcon";
 
 function Footer() {
   return (
     <footer className="p-20 desktop:p-28 flex flex-col items-center gap-14">
       <img src={darkLogo} alt="Logo image" />
 
-      <div className="flex justify-center gap-7">
-        <img src={facebookIcon} alt="Facebook icon" className="cursor-pointer" />
-        <img src={twitterIcon} alt="Twitter icon" className="cursor-pointer" />
-        <img src={instagramIcon} alt="Instagram icon" className="cursor-pointer" />
+      <div className="flex justify-center items-center gap-7">
+        <FacebookIcon />
+        <TwitterIcon />
+        <InstagramIcon />
       </div>
     </footer>
   )
