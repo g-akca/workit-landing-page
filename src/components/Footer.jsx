7ea3305b7b318c4beba31bb79969e738124a0116
g-akca <gok.akca66@gmail.com>
2026-04-05 13:09:03 +0300

    Add Footer structure and styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,19 @@
+import darkLogo from "/images/logo-dark.svg";
+import facebookIcon from "/images/icon-facebook.svg";
+import instagramIcon from "/images/icon-instagram.svg";
+import twitterIcon from "/images/icon-twitter.svg";
+
 function Footer() {
   return (
-    <>
-    </>
+    <footer className="p-20 flex flex-col items-center gap-14">
+      <img src={darkLogo} alt="Logo image" />
+
+      <div className="flex justify-center gap-7">
+        <img src={facebookIcon} alt="Facebook icon" className="cursor-pointer" />
+        <img src={twitterIcon} alt="Twitter icon" className="cursor-pointer" />
+        <img src={instagramIcon} alt="Instagram icon" className="cursor-pointer" />
+      </div>
+    </footer>
   )
 }
 
